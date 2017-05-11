e6d1a0d13a0a32124042ec6f83f6ada6d79afda5
Pepe Valdivia <jvaldivia@softweb.pe>
2017-05-10 21:47:50 -0500

    CRUD de Módulos, Subtitulos e Items asociados a un Sistema

--- assets/accesos/sistema/js/index.js
+++ assets/accesos/sistema/js/index.js
@@ -3,7 +3,7 @@ var array_json_th = [
 	{titulo:"Nombre",index:"nombre",estilos:"width: 250px;"},
     {titulo:"Versión",index:"version",estilos:"width: 100px;"},
     {titulo:"Repositorio",index:"repositorio",estilos:"width: 250px;"},
-	{titulo:"Botones",index:"NA",estilos:"width: 10px;"}
+	{titulo:"Botones",index:"NA",estilos:"width: 80px;"}
 ];
 
 var array_json_td = [
@@ -15,7 +15,10 @@ var array_json_td = [
 ];
 
 var array_json_btn_td = [
-	{clase:"fa fa-times",url:"#",alt:"Eliminar capa",estilos:"padding-left: 32px;", operacion:"QuitarFila"}
+	{clase:"fa fa-chevron-right",url:"#",alt:"Gestionar Menú",estilos:"padding-left: 2px;", operacion:"GestionarMenu"},
+	{clase:"fa fa-list",url:"#",alt:"Gestionar Permisos",estilos:"padding-left: 2px;", operacion:"GestionarPermisos"},
+	{clase:"fa fa-id-card-o",url:"#",alt:"Gestionar Roles",estilos:"padding-left: 2px;", operacion:"GestionarRoles"},
+	{clase:"fa fa-times",url:"#",alt:"Eliminar capa",estilos:"padding-left: 2px;", operacion:"QuitarFila"}
 ]; 
 
 var array_json_btn = [
@@ -37,3 +40,127 @@ tablaSistemas.SetLabelMensaje("#txtMensajeRpta");
 tablaSistemas.SetURLGuardar(BASE_URL + "accesos/sistema/guardar");
 
 tablaSistemas.MostrarTable();
+
+var GestionarMenu = new Class({
+    Interfaces: [ IChainOperacion ],
+    SetearSiguienteInstancia: function(instancia){
+        //implementación de IChainOperacion
+        this.siguiente_instancia = instancia;
+    },
+    SiguienteEslabon: function(operacion, thisDOM, objeto) {
+        //implementación de IChainOperacion
+        this.siguiente_instancia.EjecutarOperacion(operacion, thisDOM, objeto);
+    }, 
+    EjecutarOperacion: function(operacion, thisDOM, objeto) {
+        //implementación de IChainOperacion
+        if(operacion == "GestionarMenu"){
+        	var id_sistema = thisDOM.parent().parent().children().eq(0).children().eq(0).html();
+	       $.ajax({
+				type: "GET",
+				url: BASE_URL + "accesos/sistema/ver_menu",
+				data: "data=",
+				async: false,
+				success:function(data){
+					$("#modal-container").empty();
+					$("#modal-container").append(data);
+					$("#sistema_id").html(id_sistema);
+					$("#btnModal").click();
+				}
+			});
+           //ObservadorConcreto.NotificarObservadores(objeto.observador, tipo_arreglo, id_fila);
+        }else{
+             try {
+              this.SiguienteEslabon(operacion, thisDOM, objeto);
+           }catch(error){
+              console.log("Operación no implementada");
+           }
+        }
+    }
+});
+
+var GestionarPermisos = new Class({
+    Interfaces: [ IChainOperacion ],
+    SetearSiguienteInstancia: function(instancia){
+        //implementación de IChainOperacion
+        this.siguiente_instancia = instancia;
+    },
+    SiguienteEslabon: function(operacion, thisDOM, objeto) {
+        //implementación de IChainOperacion
+        this.siguiente_instancia.EjecutarOperacion(operacion, thisDOM, objeto);
+    }, 
+    EjecutarOperacion: function(operacion, thisDOM, objeto) {
+        //implementación de IChainOperacion
+        if(operacion == "GestionarPermisos"){
+        	var id_sistema = thisDOM.parent().parent().children().eq(0).children().eq(0).html();
+	       $.ajax({
+				type: "GET",
+				url: BASE_URL + "accesos/sistema/ver_permisos",
+				data: "data=",
+				async: false,
+				success:function(data){
+					$("#modal-container").empty();
+					$("#modal-container").append(data);
+					$("#sistema_id").html(id_sistema);
+					$("#btnModal").click();
+				}
+			});
+           //ObservadorConcreto.NotificarObservadores(objeto.observador, tipo_arreglo, id_fila);
+        }else{
+             try {
+              this.SiguienteEslabon(operacion, thisDOM, objeto);
+           }catch(error){
+              console.log("Operación no implementada");
+           }
+        }
+    }
+});
+
+var GestionarRoles = new Class({
+    Interfaces: [ IChainOperacion ],
+    SetearSiguienteInstancia: function(instancia){
+        //implementación de IChainOperacion
+        this.siguiente_instancia = instancia;
+    },
+    SiguienteEslabon: function(operacion, thisDOM, objeto) {
+        //implementación de IChainOperacion
+        this.siguiente_instancia.EjecutarOperacion(operacion, thisDOM, objeto);
+    }, 
+    EjecutarOperacion: function(operacion, thisDOM, objeto) {
+        //implementación de IChainOperacion
+        if(operacion == "GestionarRoles"){
+        	var id_sistema = thisDOM.parent().parent().children().eq(0).children().eq(0).html();
+	       $.ajax({
+				type: "GET",
+				url: BASE_URL + "accesos/sistema/ver_roles",
+				data: "data=",
+				async: false,
+				success:function(data){
+					$("#modal-container").empty();
+					$("#modal-container").append(data);
+					$("#sistema_id").html(id_sistema);
+					$("#btnModal").click();
+				}
+			});
+           //ObservadorConcreto.NotificarObservadores(objeto.observador, tipo_arreglo, id_fila);
+        }else{
+             try {
+              this.SiguienteEslabon(operacion, thisDOM, objeto);
+           }catch(error){
+              console.log("Operación no implementada");
+           }
+        }
+    }
+});
+
+$(document).on("click", ".mootools", function() {
+    var objeto = eval(this.get("objeto"));
+    var eslabon_1 = new GestionarMenu();
+    var eslabon_2 = new GestionarPermisos();
+    var eslabon_3 = new GestionarRoles();
+    var operacion = this.get("operacion"); console.log(operacion);
+
+	 eslabon_1.SetearSiguienteInstancia(eslabon_2);
+    eslabon_2.SetearSiguienteInstancia(eslabon_3);
+
+    eslabon_1.EjecutarOperacion(operacion, $(this), objeto);
+});
