c72b8978897eae8d0f9db563e55ad635365187da
Pepe Valdivia <jvaldivia@softweb.pe>
2017-05-24 20:39:10 -0500

    CRUD de proveedores incluyendo imágenes anexas

--- assets/administracion/proveedor/js/proveedor.js
+++ assets/administracion/proveedor/js/proveedor.js
@@ -15,6 +15,8 @@ $( "#btnGuardarEmpresa" ).click(function(event) {
     proveedor.ruc= $("#txtRuc").val();
     proveedor.direccion= $("#txtDireccion").val();
     proveedor.distrito_id= $("#idDistrito").html();
+    proveedor.imagen_dni_id= $("#imagen_dni_id").html();
+    proveedor.imagen_ruc_id= $("#imagen_ruc_id").html();
 
     var ajax_empresa = new AjaxRuby();
     ajax_empresa.Constructor ("POST", BASE_URL + "administracion/proveedor/guardar", JSON.stringify(proveedor), false);
@@ -39,35 +41,63 @@ $( "#btnGuardarEmpresa" ).click(function(event) {
 });
 
 $("#upload_file_dni").on("click", function(event){
+    $("#view_file_dni").addClass("oculto");
     $("#input_file_dni").upload(
         BASE_URL + "administracion/proveedor/subir_dni",
         {
             nombre : "Imagen 1 nombre",
             descripcion : "Imagen 1 descripcion"
         }, 
-        function(success){
-            console.log(success);
-            $("#imagen_dni_id").html(success);
+        function(mensaje){
+            console.log(mensaje);
+            if(mensaje['tipo_mensaje'] == 'success'){
+               $("#imagen_dni_id").html(mensaje['mensaje'][1]);  
+               $("#txtMensajeRptaImagenes").html(mensaje['mensaje'][0]);
+              $("#txtMensajeRptaImagenes").removeClass("color-rojo");
+              $("#txtMensajeRptaImagenes").addClass("color-success");
+              $("#view_file_dni").removeClass("oculto");
+              $("#view_file_dni").attr("href", mensaje['mensaje'][2]);
+            }else{ 
+              $("#txtMensajeRptaImagenes").removeClass("color-success");
+               $("#txtMensajeRptaImagenes").addClass("color-rojo");
+              $("#txtMensajeRptaImagenes").html(mensaje['mensaje'][0]);
+            }
+
             event.preventDefault();
         },  
-        $("#progbar_dni")
+        $("#progbar_dni"),
+        $("#upload_file_dni")
     );
     event.preventDefault();
 });
 
 $("#upload_file_ruc").on("click", function(event){
+    $("#view_file_ruc").addClass("oculto");
     $("#input_file_ruc").upload(
         BASE_URL + "administracion/proveedor/subir_ruc",
         {
             nombre : "Imagen 1 nombre",
             descripcion : "Imagen 1 descripcion"
         }, 
-        function(success){
-            console.log(success);
-            $("#imagen_ruc_id").html(success);
+        function(mensaje){
+            console.log(mensaje);
+            if(mensaje['tipo_mensaje'] == 'success'){ 
+               $("#imagen_ruc_id").html(mensaje['mensaje'][1]);  
+               $("#txtMensajeRptaImagenes").html(mensaje['mensaje'][0]);
+               $("#txtMensajeRptaImagenes").removeClass("color-rojo");
+              $("#txtMensajeRptaImagenes").addClass("color-success"); 
+              $("#view_file_ruc").removeClass("oculto");
+              $("#view_file_ruc").attr("href", mensaje['mensaje'][2]);
+            }else{
+              $("#txtMensajeRptaImagenes").removeClass("color-success");
+               $("#txtMensajeRptaImagenes").addClass("color-rojo");
+              $("#txtMensajeRptaImagenes").html(mensaje[0]);
+            }
+
             event.preventDefault();
         },  
-        $("#progbar_ruc")
+        $("#progbar_ruc"),
+        $("#upload_file_ruc")
     );
     event.preventDefault();
 });
